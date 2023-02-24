a10e80ad863f0b54c7c38b1ec180fa43d67ca34f
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-24 16:24:05 +0530

    Add : Program.cs File

--- Program.cs
+++ Program.cs
@@ -6,41 +6,42 @@ namespace StackAndQueue
     {
         public static void Main(string[] args)
         {
+            LinkedList list= new LinkedList();
             Stack stack = new Stack();
             stack.Push(70);
             stack.Push(30); 
             stack.Push(56);
 
-            foreach (var i in stack)
+            foreach (int i in stack)
             {
-                Console.WriteLine("Before Pop : " + i);
+                list.Add(i);
             }
             Console.WriteLine("\nTop of the stack : {0}",stack.Peek());
             Console.WriteLine("********************");
            
             stack.Pop();
 
-            foreach (var i in stack)
+            foreach (int i in stack)
             {
-                Console.WriteLine("After Pop : " + i);
+                list.Add(i);
             }
             Console.WriteLine("\nTop of the stack : {0}", stack.Peek());
             Console.WriteLine("********************");
 
+
             stack.Pop();
 
-            foreach (var i in stack)
+            foreach (int i in stack)
             {
-                Console.WriteLine("After Pop : " + i);
+                list.Add(i);
             }
             Console.WriteLine("\nTop of the stack : {0}", stack.Peek());
             Console.WriteLine("********************");
 
-            stack.Pop();
 
-            foreach (var i in stack)
+            foreach (int i in stack)
             {
-                Console.WriteLine("After Pop : " + i);
+                list.Add(i);
             }
             stack.Pop();
             Console.WriteLine("Stack Was Empty");
