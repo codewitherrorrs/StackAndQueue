4a33364715e9e434e59029794501d9966c56e1a2
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-24 10:36:13 +0530

    Add : Stack Push

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace StackAndQueue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30); 
+            stack.Push(56);
+
+            foreach(var i in stack)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n Top of stack : {0}",stack.Peek());
+        }
+    }
+}
